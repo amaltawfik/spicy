60d7f97f90a46e134ce70fa2ad37691e86ef12c3
amaltawfik <amal.tawfik@hesav.ch>
2025-03-21 16:51:19 +0100

    add sum_n

--- R/spicy-package.R
+++ R/spicy-package.R
@@ -3,9 +3,14 @@
 
 ## usethis namespace: start
 #' @importFrom collapse qTBL
+#' @importFrom dplyr everything
+#' @importFrom dplyr pick
 #' @importFrom dplyr pull
+#' @importFrom dplyr select
+#' @importFrom dplyr where
 #' @importFrom rlang enquo
 #' @importFrom rlang eval_tidy
+#' @importFrom rlang inform
 #' @importFrom rlang quo_is_null
 #' @importFrom stats na.omit
 #' @importFrom tibble as_tibble
